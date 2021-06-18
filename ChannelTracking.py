a0aa55178f021bcb211fc2dba091113fbaae3672
marceloslo <marceloslo@users.noreply.github.com>
2021-06-18 12:18:18 -0300

    Update ChannelTracking.py

--- ChannelTracking.py
+++ ChannelTracking.py
@@ -22,7 +22,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 while True:
     
-    with open('channels_metadata.json') as json_file:
+    with open('/home/marcelo/jsons/channels_metadata.json') as json_file:
             channelsJ=[]
             for line in json_file:
                 channelsJ.append(json.loads(line))
@@ -83,7 +83,7 @@ while True:
                     add2.append(np.nan)
                     rmChannel.loc[len(rmChannel)]=add
                     track.loc[len(track)]=add2
-    track.to_csv('channelsTracking.csv',index=False)
-    rmChannel.to_csv('removedChannels.csv',index=False)
+    track.to_csv('/home/marcelo/jsons/channelsTracking.csv',index=False)
+    rmChannel.to_csv('/home/marcelo/jsons/removedChannels.csv',index=False)
     print('done ',day)
     time.sleep(86400)
